16464ccbf289d7b5f1e6f8c63a21bbc812507e98
unknown <gersaingalindez93@gmail.com>
2023-02-21 16:44:48 -0500

    commit inicial

---
+++ src/main/java/com/sitis/gestionusuariosbackend/GestionUsuariosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sitis.gestionusuariosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuariosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsuariosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitis/gestionusuariosbackend/GestionUsuariosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitis.gestionusuariosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
